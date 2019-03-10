42dcc57f48c3583a7367444b655572479ea436dd
justbegginer <46086383+justbegginer@users.noreply.github.com>
2019-03-10 16:40:14 +0300

    last_version

---
+++ timer/timer/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timer
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+        }
+
+        private void TimerWorking(object sender, EventArgs e)
+        {
+            counter++;
+            label1.Text = Convert.ToString(counter);
+        }
+    }
+}
